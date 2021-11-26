eb24837f6a198a45764337ba32f0a13570ee9014
MedoX67 <93802008+MedoX67@users.noreply.github.com>
2021-11-26 17:31:03 +0200

    Add files via upload

--- k67.py
+++ k67.py
@@ -1,9 +1,48 @@
 import os
 os.system("clear")
+ #2v7x = 2244444
+
+#print (v7x)
+
+#hu = ('hello')
+
+#print (hu)
+
+#flo = 7.8
+
+#print (flo)
+
+#MedoX67 = input ('Type Your Name: ')
+#print ('Your Name Is: ' +MedoX67)
+
+#num_one = 20000
+#num_two = 677
+#print (num_one*num_two)
+
+#k68 = ("Hello World")
+#print (len (k68))
+#print (k68 [0:12])
+#print (k68 *1000)
+#ho = ("😲🤬😡😈👿😡👺👹😡🤬👺💥💥😖😈💥👹🤬❤️🤓🤣💥😲👿😡💥💥")
+#print (ho*10)
+#name = input ("What's Your Name: ")
+#if name == "Medo" : 
+     #print ("nice name")
+     #print ("thank you")
+
+#else:
+    #print ("Type 'v7x' please....... " )
+    #input ("=====>>>")
+    #name = "v7x"
+    #if name == "v7x" : 
+       #print ("thanks....done....")
+       #print ("welcome to k64 tool....")
+v = 0
+#while v < 11 :
+     #print ( v *5)
+     #v=v+1   
+  
+#  for v in range (10) :
+      #print ("k")
+       
  
-v7x = 2244444
-print (v7x)
-hu = ('hello')
-print (hu)
-flo = 7.8
-print (flo)
